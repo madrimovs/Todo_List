09fef40aca97358fa86956e309bae1f927b783c0
MadrimovS <sardormadrimov516@gmail.com>
2022-12-28 21:38:55 +0500

    main

--- js/app.js
+++ js/app.js
@@ -1,35 +1,3 @@
-// const elForm = document.querySelector("#form");
-// const elList = document.querySelector("#todo-list");
-// const elError = document.querySelector("#error");
-// const elClearBtn = document.querySelector("#clear-btn");
-
-// if (elError.textContent === "") {
-//   elError.textContent = "No todos yet";
-// }
-// // console.log(elError.textContent);
-// elForm.addEventListener("submit", function (evt) {
-//   evt.preventDefault();
-//   const input = evt.target.input;
-
-//   if (input.value !== "") {
-//     elError.textContent = "";
-
-//     const elTodo = document.createElement("li");
-//     elTodo.textContent = input.value;
-//     elTodo.className = "rounded ps-3 shadow bg-warning mt-2 text-dark";
-//     elList.appendChild(elTodo);
-//     input.value = "";
-//     elClearBtn.style.display = "block";
-//   } else {
-//     elError.textContent = "Please enter a todo";
-//   }
-// });
-
-// elClearBtn.addEventListener("click", function () {
-//   elList.textContent = "";
-//   elClearBtn.style.display = "none";
-// });
-
 function findElement(element, parent = document) {
   return parent.querySelector(element);
 }
@@ -38,44 +6,43 @@ const elInput = findElement("#input");
 const elClearBtn = findElement("#clearBtn");
 const elList = findElement("#list-group");
 const elError = findElement("#error");
+const products = [];
 
 if (elError.textContent === "") {
   elError.textContent = "No todos yet";
 }
+
 elForm.addEventListener("submit", function (evt) {
   evt.preventDefault();
 
+  //add to array
   if (elInput.value !== "") {
     elError.textContent = "";
 
-    const elTodo = document.createElement("li");
-    elTodo.classList = "bg-light rounded ps-3 text-dark mt-2";
-    elTodo.textContent = elInput.value;
-
-    elList.appendChild(elTodo);
+    products.push(elInput.value);
 
     elClearBtn.style.display = "block";
-    products.push(input.value);
-
     elInput.value = "";
+
+    //render array
+    elList.textContent = "";
+
+    for (let i = 0; i < products.length; i++) {
+      const element = products[i];
+      const elLi = document.createElement("li");
+
+      elLi.textContent = element;
+      elLi.className = "bg-light rounded ps-3 text-dark mt-2";
+      elList.appendChild(elLi);
+    }
   } else {
     elError.textContent = "Please enter a todos";
   }
+  console.log(products);
 });
+
 //Clear button
 elClearBtn.addEventListener("click", function () {
   elList.textContent = "";
   elClearBtn.style.display = "none";
 });
-
-//array
-const products = [];
-
-for (let i = 0; i < products.length; i++) {
-  const element = products[i];
-  const elLi = document.createElement("li");
-
-  elLi.textContent = element;
-  elLi.className = "bg-light rounded ps-3 text-dark mt-2";
-  elList.appendChild(elLi);
-}
